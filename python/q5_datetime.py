979af8bb7b1f3bb26d3facbabcba3ff33b4c961d
SanghamitraDutta <sanghamitradey@gmail.com>
2017-07-25 16:02:25 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -15,10 +15,31 @@ print(b)
 print(delta.days)
 
 
-####b)  
+####b) 
+from datetime import datetime 
+
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015' 
+
+a=datetime.strptime(date_start,'%m%d%Y')
+b=datetime.strptime(date_stop,'%m%d%Y')
+
+delta=b-a
+print(a)
+print(b)
+print(delta.days)
+
 
 ####c)  
+from datetime import datetime 
+
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+a=datetime.strptime(date_start,'%d-%b-%Y')
+b=datetime.strptime(date_stop,'%d-%b-%Y')
+
+delta=b-a
+print(a)
+print(b)
+print(delta.days)
