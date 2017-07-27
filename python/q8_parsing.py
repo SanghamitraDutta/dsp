fbcd024c4587ab50a595196287a3e1e59932073f
SanghamitraDutta <sanghamitradey@gmail.com>
2017-07-27 16:40:48 -0400

    All questions answered

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -3,7 +3,6 @@
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
-
 import csv
 from operator import itemgetter
 
