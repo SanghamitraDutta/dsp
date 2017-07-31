f764fb1854b3ebeb8a7254f48d914b76d2faf763
SanghamitraDutta <sanghamitradey@gmail.com>
2017-07-31 16:08:25 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
 PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv') as f:
+    data = list(csv.DictReader(f))
+    list1 = [ dict(x) for x in data ]
+  
+# Creating a list of all the email addressess   
+list_all_emails = [ x[' email'] for x in list1]    
+print("\nList of all email addresses: \n",list_all_emails,'\n')
+
+# Creating a new file 'email.csv and writing the list of emails in the new file
+with open('email.csv', 'w') as fwrite:
+    data_writer = csv.writer(fwrite, delimiter='\n')
+    data_writer.writerow(list_all_emails)
