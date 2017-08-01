ac35c9d6ce0333fa357ea6c8ff4ff6990c57002a
SanghamitraDutta <sanghamitradey@gmail.com>
2017-08-01 15:04:19 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -16,3 +16,21 @@ for x in list1:
         faculty_dict[x['name'].split()[-1]].append([x[' degree'],x[' title'][:-17],x[' email']])                      
                               
 print(faculty_dict)
+
+
+# Q7
+
+import csv
+
+with open('faculty.csv') as f:
+    data = list(csv.DictReader(f))
+    list1 = [ dict(x) for x in data ]        
+
+# Creating the dictionary in the specified format
+                   
+professor_dict = { (x['name'].split()[0],x['name'].split()[-1]):[x[' degree'], x[' title'][:-17], x[' email']] for x in list1 }
+                   
+print(professor_dict)
+
+
+# Q8
