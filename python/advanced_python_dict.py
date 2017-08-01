197868af4ea2bd8f32a1b1a22341bdb04406dee8
SanghamitraDutta <sanghamitradey@gmail.com>
2017-08-01 14:32:35 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+# Q6
+
+import csv
+
+with open('faculty.csv') as f:
+    data = list(csv.DictReader(f))
+    list1 = [ dict(x) for x in data ]    
+ 
+# Creating the dictionary in the specified format
+
+faculty_dict = {}
+for x in list1:
+    if x['name'].split()[-1] not in faculty_dict:
+        faculty_dict.update({ x['name'].split()[-1]:[[x[' degree'], x[' title'][:-17],x[' email']]]})
+    else:
+        faculty_dict[x['name'].split()[-1]].append([x[' degree'],x[' title'][:-17],x[' email']])                      
+                              
+print(faculty_dict)
