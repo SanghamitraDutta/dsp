30befcd5d99b9d0e7d2a48df299bca4dbb12764a
SanghamitraDutta <sanghamitradey@gmail.com>
2017-08-12 14:44:15 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -21,6 +21,29 @@ for k,v in faculty_dict.items():
         print(k,':',v)
         count += 1
 
+#Q6 using re
+import csv
+import re 
+
+with open('faculty.csv') as f:
+    data = list(csv.DictReader(f))
+    list1 = [ dict(x) for x in data ]
+    '''
+    for x in list1:
+        print (x)
+    '''
+# Creating dictionary with last name as key    
+faculty_dict = {}
+for i in list1:
+    L_name = re.search(r'\b[A-Z]{1}[a-z]+$',i['name']).group()
+    if L_name in faculty_dict:
+        faculty_dict[L_name].append([i[' degree'],i[' title'],i[' email']])
+    else:
+        faculty_dict.update({ L_name : [[i[' degree'],i[' title'],i[' email']]]})
+
+for i in faculty_dict:
+    print(i,':', faculty_dict[i])
+
 
 # Q7
 import csv
@@ -31,7 +54,7 @@ with open('faculty.csv') as f:
 
 # Creating the dictionary in the specified format
                    
-professor_dict = { (x['name'].split()[0],x['name'].split()[-1]):[x[' degree'], x[' title'][:-17], x[' email']] for x in list1 }
+professor_dict = { tuple(x['name'].split()[0],x['name'].split()[-1]):[x[' degree'], x[' title'][:-17], x[' email']] for x in list1 }
   
 # Printing the first 3 key value pairs of the dicitonary    
 count=0
